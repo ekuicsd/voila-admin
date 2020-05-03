bc32aa5a1d073f3c3c941dcd61956fc3ae8ff605
ekuicsd <46596077+ekuicsd@users.noreply.github.com>
2020-05-04 01:06:10 +0530

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FullComponentComponent } from './layouts/full-component/full-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: FullComponentComponent, children: [
+      {
+        path: '', redirectTo: '/dashboard', pathMatch: 'full'
+      },
+      {
+        path: 'dashboard', component: DashboardComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './account/login-page/login-page.component';
+import { ForgotPasswordComponent } from './account/forgot-password/forgot-password.component';
+import { ChangePasswordComponent } from './account/change-password/change-password.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { FullComponentComponent } from './layouts/full-component/full-component.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    ForgotPasswordComponent,
+    ChangePasswordComponent,
+    EditProfileComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    FullComponentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgbModule,
+    PerfectScrollbarModule,
+    MDBBootstrapModule.forRoot(),
+    AppRoutingModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
